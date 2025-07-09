b1616540512a4112e2a5f0f03d9aa600f7d0a98d
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-07-09 18:49:33 +0300

    changing base path vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Nexexexexed/frontend-html-task",
+  base: "/frontend-html-task/",
   plugins: [react()],
 });
